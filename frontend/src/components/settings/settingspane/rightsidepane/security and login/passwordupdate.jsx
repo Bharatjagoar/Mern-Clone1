2b13a1734f90175e89c8aab0310d60de01e352bf
Bharat Jagoar <bharatjagoar15@gmail.com>
2022-09-19 22:08:31 +0530

    this is password form updation in change password component !!

--- frontend/src/components/settings/settingspane/rightsidepane/security and login/passwordupdate.jsx
+++ frontend/src/components/settings/settingspane/rightsidepane/security and login/passwordupdate.jsx
@@ -1,18 +1,39 @@
 import React from "react";
 import PasswordupdateCss from "./passwordupdate.module.css"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import {faKey} from "@fortawesome/free-solid-svg-icons"
+import ChangePassword from "../updation forms/passwordChange";
+import { useState } from "react";
 
 
 function Password(){
+    const [cancel,setcancel]= useState(false)
     
-        return <div className={PasswordupdateCss.outerMostdiv}>
-            <div className={PasswordupdateCss.headingContainer}>
-                <h3 className={PasswordupdateCss.heading}>Password</h3>
+    function BtnClicked(){
+        console.log("btn clicked ")
+        setcancel(false)
+    }
+    
+    return <div className={PasswordupdateCss.outerMostdiv}>
+        <div className={PasswordupdateCss.headingContainer}>
+            <h3 className={PasswordupdateCss.heading}>Password</h3>
+        </div>
+        <div className={PasswordupdateCss.content}>
+            <div className={PasswordupdateCss.contentdivone}>
+                <FontAwesomeIcon icon={faKey} className={PasswordupdateCss.KeyIcon}/>
             </div>
-            <div className={PasswordupdateCss.content}>
-                <div className={PasswordupdateCss.contentdivone}></div>
-                <div className={PasswordupdateCss.contentdivtwo}></div>
-                <div className={PasswordupdateCss.contentdivthree}></div>
+            <div className={PasswordupdateCss.contentdivtwo}>
+                {cancel?
+                <>
+                <h4>Change password</h4>
+                <p>It's a good idea to use a strong password that you don't use elsewhere</p>
+            </>:<ChangePassword funct={setcancel}/>}
+                
+            </div>
+            <div className={PasswordupdateCss.contentdivthree}>
+                {cancel&&<button onClick={()=>{BtnClicked()}}>Edit</button>}
             </div>
         </div>
+    </div>
 }
 export default Password;
--- frontend/src/components/settings/settingspane/rightsidepane/updation forms/passwordChange.jsx
+++ frontend/src/components/settings/settingspane/rightsidepane/updation forms/passwordChange.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import NameUpdateCss from "./nameupdate.module.css"
 
-function ChangePassword(){
-
+function ChangePassword({funct}){
+    function CancelFunct(){
+        funct(true)
+    }
     return <div>
-            <h1>ChangePassword </h1>
             <div>
             <form method="put" action="" className={NameUpdateCss.forum}>
             <h3 className={NameUpdateCss.headingOftheForm}>Password change</h3>
@@ -12,7 +13,7 @@ function ChangePassword(){
             <input onChange={(e)=>{}} className={NameUpdateCss.inputData} type="text" placeholder="enter the last name "/>
             <div className={NameUpdateCss.BtncontainerUpdatename}>
                 <button className={NameUpdateCss.updatenameSubmit} type="button" onClick={(e)=>{}}>submit</button>
-                <button className={NameUpdateCss.updatenamecancel} type="button"> cancel</button>
+                <button className={NameUpdateCss.updatenamecancel} type="button" onClick={(e)=>{CancelFunct()}}> cancel</button>
             </div>
             
         </form>
